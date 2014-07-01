a5eabb1ca626a577277e5b047d9768024ddcfa57
Devin Dreszer <devindreszer@gmail.com>
2014-07-01 12:30:09 -0400

    Users have managed albums

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  # Added a child relationship for managed_albums
+  # This user can have one or more managed albums
+  # If it was name albums_users it would not need a class name
+  has_many :managed_albums, class_name: 'ManagedAlbums', dependent: :destroy
+
+  # has many through
+  has_many :albums, through: :managed_albums
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
